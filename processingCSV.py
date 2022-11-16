89ad7c141853db508ba1101f697b349cad3be01f
Guilherme Vieira <81487846+gui-nvieira@users.noreply.github.com>
2022-11-16 17:35:11 -0300

    Map filter and reduce applied on CSV

--- processingCSV.py
+++ processingCSV.py
@@ -1,13 +1,8 @@
 import csv
-
-file = open('./campeonato-brasileiro-full.csv', 'r', encoding='utf-8')
-data = csv.reader(file)
-
-for row in data:
-    print(row)
+from functools import reduce
 
 
-# função de teste
+# função de visualização dos valores tratados
 def show(title, fileName, f):
     print('---- '+title+' ----')
     with open(fileName, 'r', encoding='utf-8') as file:
@@ -20,3 +15,51 @@ def show(title, fileName, f):
         else:
             for row in newData:
                 print((row))
+
+
+# função para retornar todos os dados que incluem um termo dentro de certas colunas, nesse caso é um time
+def getTeam(team):
+    return lambda data: filter(lambda e: e['mandante'] == team or e['visitante'] == team, data)
+# teste     -show('getTeam', './campeonato-brasileiro-full.csv', getTeam('Fluminense'))
+
+
+# função para retornar o ano de cada jogo
+def getYear(match):
+    dateStr = match['data']
+    yearStr = dateStr[:4]
+    return int(yearStr)
+
+
+# função para adicionar ano em uma cópia do dicionário original
+def addYearField(match):
+    newMatch = dict(match)
+    newMatch['ano'] = getYear(match)
+    return newMatch
+
+
+# função para criar o novo dict, usando o getYear
+def addYear(data): return map(addYearField, data)
+# teste   -show('addYear', './campeonato-brasileiro-full.csv', addYear)
+
+
+# função para mapear a sequencia de jogos em uma lista de gols
+def match2goals(d):
+    return map(lambda e: int(e['mandante_placar']) + int(e['visitante_placar']), d)
+
+
+# função para contar o número de gols totais no csv
+def countGoals(data):
+    listGoals = match2goals(data)
+    return reduce(lambda acc, e: acc + e, listGoals, 0)
+# teste   -show('goals', './campeonato-brasileiro-full.csv', countGoals)
+
+
+# função para retornar os jogos de um determinado ano
+def filterYear(year):
+    return lambda data: filter(lambda e: getYear(e) == year, data)
+#teste  -show('filterYear', './campeonato-brasileiro-full.csv', filterYear(2005))
+
+
+# inicializador
+if __name__ == "__main__":
+    show('filterYear', './campeonato-brasileiro-full.csv', filterYear(2005))
